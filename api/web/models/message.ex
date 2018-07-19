41128509ae7de42c4445ce0446fb231e715108ed
Trevor Delamorandiere <tdelam@gmail.com>
2018-07-19 12:51:22 -0400

    sockets

--- api/web/models/message.ex
+++ api/web/models/message.ex
@@ -9,12 +9,9 @@ defmodule Sling.Message do
     timestamps()
   end
 
-  @doc """
-  Builds a changeset based on the `struct` and `params`.
-  """
   def changeset(struct, params \\ %{}) do
     struct
-    |> cast(params, [:text])
-    |> validate_required([:text])
+    |> cast(params, [:text, :user_id, :room_id])
+    |> validate_required([:text, :user_id, :room_id])
   end
 end
